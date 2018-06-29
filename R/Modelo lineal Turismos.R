e0f73b31e6b10bb84de9e0340257e5bd450113ca
franciscojdiezc <37455238+franciscojdiezc@users.noreply.github.com>
2018-06-29 09:34:26 +0200

    Add files via upload

--- R/Modelo lineal Turismos.R
+++ R/Modelo lineal Turismos.R
@@ -28,6 +28,8 @@ Andalucia$Predict = predict(modeloandalucia, type = 'response')
 plot(Andalucia$PIB, Andalucia$Turismos, xlab = "PIB", ylab = "Turismos")
 abline(modeloandalucia)
 
+Andalucia = cbind(Andalucia,predict(modeloandalucia, Andalucia, interval = 'prediction', level = 0.90))
+
 #MADRID
 
 Madrid = turismos[turismos$Comunidad=="MADRID, COMUNIDAD DE",]
@@ -38,6 +40,8 @@ plot(Madrid$PIB, Madrid$Turismos, xlab = "PIB", ylab = "Turismos")
 abline(modelomadrid)
 
 Madrid$Predict = predict(modelomadrid, type = 'response')
+
+Madrid = cbind(Madrid, predict(modelomadrid, Madrid, interval = 'prediction', level = 0.95))
 
 #ESTIMACIONES PIB
 
@@ -54,14 +58,21 @@ estimaciones2018$PIB = estimaciones2018$PIB * ((estimaciones2018$var/100)+1)
 prediccionmadrid = estimaciones2018[estimaciones2018$Comunidad == 'MADRID, COMUNIDAD DE',c(1,3)]
 
 predict(modelomadrid, prediccionmadrid) #455111.7
+intervalo = predict(modelomadrid, prediccionmadrid,interval = "prediction", level = 0.90)
+
+confint(modelomadrid, level = 0.95)
 
 prediccionmadrid$predict = predict(modelomadrid, prediccionmadrid)
+
+prediccionmadrid = cbind(prediccionmadrid, intervalo)
 
 #PREDICCIÓN ANDALUCIA
 
 prediccionandalucia = estimaciones2018[estimaciones2018$Comunidad == 'ANDALUCÍA',c(1,3)]
 
-predict(modelomadrid, prediccionmadrid)  #207719.7
+predict(modeloandalucia, prediccionandalucia)  #207719.7
+predict(modeloandalucia, prediccionandalucia, interval = 'prediction', level = 0.9)
 
 prediccionandalucia$predict = predict(modeloandalucia, prediccionandalucia)
+
 
